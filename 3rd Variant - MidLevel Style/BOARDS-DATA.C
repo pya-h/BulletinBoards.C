cfb310d5d93ea60f69bac1e65896c97ff35abcc9
pya-h <p4ya.py@gmail.com>
2024-01-29 01:21:42 +0330

    Save file one part completed. Reallocation implemented

--- 3rd Variant - MidLevel Style/BOARDS-DATA.C
+++ 3rd Variant - MidLevel Style/BOARDS-DATA.C
@@ -1,7 +1,6 @@
 #include "BOARDS-DATA.H"
 
-
-void SetImportance(struct TASK *Task)
+void    SetImportance(struct TASK *Task)
 {
     // Harf avval ->Importance Ra Check mikonad va Meghdar Motabar daronash Gharar Midahad
     Task->Importance[0] = toupper(Task->Importance[0]);
@@ -18,7 +17,26 @@ void SetImportance(struct TASK *Task)
         strcpy(Task->Importance, "Low"); // Kamel kardane matn Importance
     }
 }
-void WriteData(char *UserName, char *Password, struct BOARD Boards[], int End)
+
+void    CheckBoardCapacityForLists(struct BOARD Boards[], int BoardNumber)
+{
+    if (Boards[BoardNumber].Li >= Boards[BoardNumber].Capacity)
+    {
+        Boards[BoardNumber].Capacity += 10;
+        Boards[BoardNumber].Lists = (struct LIST *)realloc(Boards[BoardNumber].Lists, Boards[BoardNumber].Capacity);
+    }
+}
+
+void    CheckListCapacityForTasks(struct BOARD Boards[], int BoardNumber, int ListNumber)
+{
+    if (Boards[BoardNumber].Lists[ListNumber].Ti >= Boards[BoardNumber].Lists[ListNumber].Capacity)
+    {
+        Boards[BoardNumber].Lists[ListNumber].Capacity += 50;
+        Boards[BoardNumber].Lists[ListNumber].Tasks = (struct TASK *)realloc(Boards[BoardNumber].Lists[ListNumber].Tasks, Boards[BoardNumber].Lists[ListNumber].Capacity);
+    }
+}
+
+void    WriteData(char *UserName, char *Password, struct BOARD Boards[], int End)
 {
     FILE *DataCsvFile = fopen("DATA.CSV", "w");
 
@@ -33,7 +51,7 @@ void WriteData(char *UserName, char *Password, struct BOARD Boards[], int End)
     // USER     2     user1   pass1     // yani in user 2 Board darad
     // BOARD    2       board1          // yani in board ham 2 ZirMajmoe(List) Darad
     //  LIST    0       list1           // In List Taski nadarad
-    //  LIST    1       list2           
+    //  LIST    1       list2
     //  TASK    0       task1       High    3   5   2024
     // ...
     // USER     user2   pass2       // Va Ela Akhar
@@ -45,16 +63,16 @@ void WriteData(char *UserName, char *Password, struct BOARD Boards[], int End)
         // Write a Bakhsh Board ha
         fprintf(DataCsvFile, "BOARD,%d,%s\n", Boards[i].Li, Boards[i].Name);
 
-        //Write a List a har Board
-        for(int j = 0; j < Boards[i].Li; j++)
+        // Write a List a har Board
+        for (int j = 0; j < Boards[i].Li; j++)
         {
-            fprintf(DataCsvFile, "LIST,%d,%s\n", Boards[i].Lists[j].Name);
+            fprintf(DataCsvFile, "LIST,%d,%s\n", Boards[i].Lists[j].Ti, Boards[i].Lists[j].Name);
 
             // write a TASK haye har LIST
-            for(int k = 0; k < Boards[i].Lists[j].Ti; k++)
+            for (int k = 0; k < Boards[i].Lists[j].Ti; k++)
             {
-                fprintf(DataCsvFile, "TASK,0,%s,%s,%d,%d,%d\n", Boards[i].Lists[j].Tasks[k].Name, Boards[i].Lists[j].Tasks[k].Importance,  // TASK ha Sub Item Nadarand.
-                    Boards[i].Lists[j].Tasks[k].Deadline.D, Boards[i].Lists[j].Tasks[k].Deadline.M, Boards[i].Lists[j].Tasks[k].Deadline.Y);
+                fprintf(DataCsvFile, "TASK,0,%s,%s,%d,%d,%d\n", Boards[i].Lists[j].Tasks[k].Name, Boards[i].Lists[j].Tasks[k].Importance, // TASK ha Sub Item Nadarand.
+                        Boards[i].Lists[j].Tasks[k].Deadline.D, Boards[i].Lists[j].Tasks[k].Deadline.M, Boards[i].Lists[j].Tasks[k].Deadline.Y);
             }
         }
     }
--- 3rd Variant - MidLevel Style/MAIN.C
+++ 3rd Variant - MidLevel Style/MAIN.C
@@ -5,35 +5,15 @@
 #include <ctype.h>
 #include <conio.h>
 
-void ShowBoards(struct BOARD Boards[], int End)
-{
-    puts("\n");
-    for (int i = 0; i < End; i++)
-    {
-        printf(" %d) %s\n", i, Boards[i].Name);
-    }
-}
+void    ShowBoards(struct BOARD Boards[], int End);
 
-void ShowLists(struct BOARD Boards[], int Bn) // Bn = Board Number
-{
-    puts(""); // Next Line
-    for (int i = 0; i < Boards[Bn].Li; i++)
-    {
-        printf(" %d) %s\n", i, Boards[Bn].Lists[i].Name);
-    }
-}
+void    ShowLists(struct BOARD Boards[], int Bn); // Bn = Board Number
 
-void ShowTasks(struct LIST Lists[], int Ln) // Ln = List Number
-{
-    puts("");
-    for (int i = 0; i < Lists[Ln].Ti; i++)
-    {
-        printf(" %d) %s, Importance: %s, Deadline: %d/%d/%d\n", i, Lists[Ln].Tasks[i].Name, Lists[Ln].Tasks[i].Importance,
-               Lists[Ln].Tasks[i].Deadline.D, Lists[Ln].Tasks[i].Deadline.D, Lists[Ln].Tasks[i].Deadline.Y);
-    }
-}
+void    ShowTasks(struct LIST Lists[], int Ln); // Ln = List Number
 
-int main()
+void    ShowOneTask(struct TASK Task);
+
+int     main()
 {
     char UserName[] = "test"; // Agar In Meghdar Begirad Yani Karbar Vorod Movafagh Dashte /.
     char Password[] = "test";
@@ -68,58 +48,58 @@ int main()
         }
         else
         {
-            puts("[S]HOW BOARDS  [A]DD BOARD  [G]OTO BOARD  ?");
-            char Input[1000];
-            gets(Input);
-            if (Input[1] != NULL)
+            char Selection;
+            if (CurrentBoardNumber < 0)
             {
-                puts("Invalid Selection. Please Try Again...\n");
-                continue;
+                puts("[S]HOW BOARDS  [A]DD BOARD  [G]OTO BOARD  ?");
+                Selection = getche();
             }
-            char Selection = Input[0];
+            else
+            {
+                Selection = 'g';
+            }
             Selection = tolower(Selection); // Momken ast CAPS LOCK Karbar Roshan Bashad. Intori Dar Har Do Halat Vorodi Motabar Khahad Bood/.
-            if (Selection == 'a')
-            {
-                puts("Enter Board Name:\n");
-                gets(Boards[Bi].Name);
-                Boards[Bi].Capacity = 100;
-                Boards[Bi].Lists = (struct LIST *)malloc(sizeof(struct LIST) * Boards[Bi].Capacity);
-                puts("Done.");
-                Bi++;
-                WriteData(UserName, Password, Boards, Bi);
-            }
-            else if (Selection == 's')
-            {
-                ShowBoards(Boards, Bi);
-            }
-            else if (Selection == 'g')
+            puts("");
+            if (Selection == 'g')
             {
                 int Bn = 0;
-                ShowBoards(Boards, Bi);
-                puts("Select: ");
-                scanf("%d%*c", &Bn);
-                if (Bn < 0 || Bn >= Bi)
+                if (CurrentBoardNumber < 0) // In Bakhsh barayeh In Ast Ke Agar Karbar MAsalan Be Board 1 om Raft, Dar Marheleye Ba'd Dobareh
+                // Be Safheyeh Entekhab Board Bar Nagardad
+                // Daghighan CurrentListNumber Hamin Kar Ra Barayeh List ha mikonad
                 {
-
-                    puts("Board Number Invalid! Try Again Later.\n");
-                    continue; // Bargard Be Menu Asli
+                    ShowBoards(Boards, Bi);
+                    printf(" => Select: ");
+                    scanf("%d%*c", &Bn);
+                    if (Bn < 0 || Bn >= Bi)
+                    {
+                        CurrentBoardNumber = CurrentListNumber = -1;
+                        puts("Board Number Invalid! Try Again Later.");
+                        continue; // Bargard Be Menu Asli
+                    }
+                    CurrentBoardNumber = Bn;
+                }
+                else
+                {
+                    Bn = CurrentBoardNumber;
                 }
 
                 // Shoroe'e Menu List haye in Board
                 // Hamanand Menu Ghabli, Amma Inbar Barayeh Listha
                 // system("cls");
-                puts("\nCurrent Board: ");
-                puts(Boards[Bn].Name);
-                puts(" => [E]DIT BOARD NAME  [D]ELETE BOARD  [S]HOW LISTS  [A]DD LIST  [G]OTO LIST  ?");
-
-                gets(Input);
-                if (Input[1] != NULL)
+                char Selection2;
+                if (CurrentListNumber < 0)
                 {
-                    puts("Invalid Selection. Please Try Again...\n");
-                    continue;
+                    puts("Current Board: ");
+                    puts(Boards[Bn].Name);
+                    puts(" => [E]DIT BOARD NAME  [D]ELETE BOARD  [S]HOW LISTS  [A]DD LIST  [G]OTO LIST  [P]revious  ?");
+                    Selection2 = tolower(getche());
+                    puts("");
                 }
-                char Selection2 = Input[0];
-                Selection2 = tolower(Selection2);
+                else
+                {
+                    Selection2 = 'g';
+                }
+
                 if (Selection2 == 's')
                 {
                     ShowLists(Boards, Bn);
@@ -127,12 +107,14 @@ int main()
                 else if (Selection2 == 'a')
                 {
                     puts("Enter List Name:");
+                    CheckBoardCapacityForLists(Boards, Bn);
                     gets(Boards[Bn].Lists[Boards[Bn].Li].Name);
                     Boards[Bn].Lists[Boards[Bn].Li].Ti = 0;
                     Boards[Bn].Lists[Boards[Bn].Li].Capacity = 100;
                     Boards[Bn].Lists[Boards[Bn].Li].Tasks = (struct TASK *)malloc(sizeof(struct TASK) * Boards[Bn].Lists[Boards[Bn].Li].Capacity);
                     puts("Done.");
                     Boards[Bn].Li++;
+                    WriteData(UserName, Password, Boards, Bi);
                 }
                 else if (Selection2 == 'e')
                 {
@@ -145,32 +127,36 @@ int main()
                 {
                     // Hazveh cascade
                 }
+                else if (Selection2 == 'p')
+                {
+                    CurrentBoardNumber = -1;
+                }
                 else if (Selection2 == 'g')
                 {
                     int Ln = 0;
-                    ShowLists(Boards, Ln);
-                    puts("Select: ");
-                    scanf("%d%*c", &Ln);
-                    if (Ln < 0 || Ln >= Boards[Bn].Li)
+                    if (CurrentListNumber < 0)
                     {
-                        puts("List Number Invalid! Try Again Later.\n");
-                        continue; // Bargard Be Menu Asli
+                        ShowLists(Boards, Bn);
+                        printf("Select: ");
+                        scanf("%d%*c", &Ln);
+                        if (Ln < 0 || Ln >= Boards[Bn].Li)
+                        {
+                            puts("List Number Invalid! Try Again Later.\n");
+                            continue; // Bargard Be Menu Asli
+                        }
+                        CurrentListNumber = Ln;
                     }
-
+                    else
+                    {
+                        Ln = CurrentListNumber;
+                    }
                     // Menu Task ha
                     // system("cls");
                     printf("\nList:%s of Board:%s\n", Boards[Bn].Lists[Ln].Name, Boards[Bn].Name);
-                    puts(" =>  [E]DIT LIST NAME  [D]ELETE LIST  [S]HOW TASKS  [A]DD TASK  [G]OTO TASK  ?");
+                    puts(" =>  [E]DIT LIST NAME  [D]ELETE LIST  [S]HOW TASKS  [A]DD TASK  [G]OTO TASK  [P]revious ?");
 
-                    gets(Input);
-                    if (Input[1] != NULL)
-                    {
-                        puts("Invalid Selection. Please Try Again...\n");
-                        continue;
-                    }
-                    char Selection3 = Input[0];
-                    Selection3 = tolower(Selection3);
-
+                    char Selection3 = tolower(getche());
+                    puts("");
                     if (Selection3 == 's')
                     {
                         ShowTasks(Boards[Bn].Lists, Ln);
@@ -191,15 +177,8 @@ int main()
                     }
                     else if (Selection3 == 'e')
                     {
-                        struct TASK *NewTask = &Boards[Bn].Lists[Ln].Tasks[Boards[Bn].Lists[Ln].Ti];
-                        puts("Edit Task Name:");
-                        gets(NewTask->Name);
-                        puts("Edit Task Importance: [H]igh [M]edium [L]ow");
-                        gets(NewTask->Importance);
-                        SetImportance(NewTask);
-                        puts("Edit Deadline D M Y");
-                        scanf("%d %d %d", &NewTask->Deadline.D, &NewTask->Deadline.M, &NewTask->Deadline.Y);
-                        gets(NewTask->Name);
+                        puts("Enter Another List Name: ");
+                        gets(Boards[Bn].Lists[Ln].Name);
                         puts("Done.");
                         WriteData(UserName, Password, Boards, Bi);
                     }
@@ -207,8 +186,100 @@ int main()
                     {
                         // Hazveh cascade
                     }
+                    else if (Selection3 == 'p')
+                    {
+                        CurrentListNumber = -1;
+                    }
+                    else if (Selection3 == 'g')
+                    {
+                        int Tn = 0; // Task number
+                        ShowTasks(Boards[Bn].Lists, Ln);
+                        printf("  =>  Select: ");
+                        scanf("%d%*c", &Tn);
+                        if (Tn < 0 || Tn >= Boards[Bn].Lists[Ln].Ti)
+                        {
+                            puts("Task Number Invalid! Please Try Again!");
+                            continue;
+                        }
+                        puts(" =>  [E]DIT TASK [D]ELETE TASK  [S]HOW THIS TASK [P]revious ?");
+
+                        char Selection4 = getche(); // get;
+                        puts("");
+                        if (Selection4 == 'e')
+                        {
+                            struct TASK *NewTask = &Boards[Bn].Lists[Ln].Tasks[Tn];
+                            puts("Edit Task Name:");
+                            gets(NewTask->Name);
+                            puts("Edit Task Importance: [H]igh [M]edium [L]ow");
+                            gets(NewTask->Importance);
+                            SetImportance(NewTask);
+                            puts("Edit Deadline D M Y");
+                            scanf("%d %d %d", &NewTask->Deadline.D, &NewTask->Deadline.M, &NewTask->Deadline.Y);
+                            gets(NewTask->Name);
+                            puts("Done.");
+                            WriteData(UserName, Password, Boards, Bi);
+                        }
+                        else if (Selection4 == 'p')
+                        {
+                            continue;
+                        }
+                    }
                 }
+            }
+            else if (Selection == 'a')
+            {
+                if (Bi >= BoardCapacity)
+                {
+                    BoardCapacity += 10;
+                    Boards = (struct BOARD *)realloc(Boards, sizeof(struct BOARD) * BoardCapacity);
+                }
+                puts("Enter Board Name:\n");
+                gets(Boards[Bi].Name);
+                Boards[Bi].Capacity = 100;
+                Boards[Bi].Lists = (struct LIST *)malloc(sizeof(struct LIST) * Boards[Bi].Capacity);
+                puts("Done.");
+                Bi++;
+                WriteData(UserName, Password, Boards, Bi);
+            }
+            else if (Selection == 's')
+            {
+                ShowBoards(Boards, Bi);
             }
         }
     }
+}
+
+void ShowBoards(struct BOARD Boards[], int End)
+{
+    puts("All Boards:");
+    for (int i = 0; i < End; i++)
+    {
+        printf(" %d) %s\n", i, Boards[i].Name);
+    }
+}
+
+void ShowLists(struct BOARD Boards[], int Bn) // Bn = Board Number
+{
+    puts("Lists of Selected Board:"); // Next Line
+    for (int i = 0; i < Boards[Bn].Li; i++)
+    {
+        printf(" %d) %s\n", i, Boards[Bn].Lists[i].Name);
+    }
+}
+
+void ShowTasks(struct LIST Lists[], int Ln) // Ln = List Number
+{
+    puts("Tasks of Selected List And Board:");
+    for (int i = 0; i < Lists[Ln].Ti; i++)
+    {
+        printf("  %d) ", i);
+        ShowOneTask(Lists[Ln].Tasks[i]);
+    }
+}
+
+void ShowOneTask(struct TASK Task)
+{
+
+    printf("%s, Importance: %s, Deadline: %d/%d/%d\n", Task.Name, Task.Importance,
+           Task.Deadline.D, Task.Deadline.D, Task.Deadline.Y);
 }
